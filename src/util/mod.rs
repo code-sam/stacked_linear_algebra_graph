f3ae515cea1b5c202c847a1fd96c46417cc2e6d8
code-sam <samdekker@gmail.com>
2022-06-08 07:50:56 +0200

    refactor: move to new module

--- src/util/mod.rs
+++
@@ -1,5 +0,0 @@
-pub(crate) mod indexed_data_store;
-
-// REVIEW; pub use only required for benchmarking
-// pub(crate) use indexed_data_store::IndexedDataStore;
-// pub use indexed_data_store::{IndexedDataStore};
