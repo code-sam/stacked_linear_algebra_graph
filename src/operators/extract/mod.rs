3588c17f9a86562ef0bd7dd09279dc9dedcccbde
Sam Dekker <samdekker@gmail.com>
2023-06-12 22:10:59 +0200

    refactor: renaming feat: implement extract edges with tail vertex

--- src/operators/extract/mod.rs
+++ src/operators/extract/mod.rs
@@ -1,3 +1,5 @@
-mod extract_edges_heading_to_vertex;
+mod extract_edges_with_head_vertex;
+mod extract_edges_with_tail_vertex;
 
-pub use extract_edges_heading_to_vertex::*;
+pub use extract_edges_with_head_vertex::*;
+pub use extract_edges_with_tail_vertex::*;
