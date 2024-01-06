7e96c1b74a74ea3c8bed73f8ed65ccfda7072450
Sam Dekker <samdekker@gmail.com>
2024-01-06 22:52:08 +0100

    chore: remove dead code

--- src/graph/value_type/mod.rs
+++ src/graph/value_type/mod.rs
@@ -1,9 +1,6 @@
-mod conversion;
 mod macro_utils;
-mod native;
 mod value_type;
 
 // TODO: remove unused modules and code
 pub(crate) use macro_utils::*;
-pub(crate) use native::*;
 pub use value_type::*;
