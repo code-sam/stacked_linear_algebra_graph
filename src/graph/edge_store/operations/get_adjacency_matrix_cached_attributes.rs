b576844e5416c45d22a445b3d0a2085d687778e0
Sam Dekker <samdekker@gmail.com>
2024-02-03 22:31:37 +0100

    fix: get transposed adjacency matrix

--- src/graph/edge_store/operations/get_adjacency_matrix_cached_attributes.rs
+++ src/graph/edge_store/operations/get_adjacency_matrix_cached_attributes.rs
@@ -3,38 +3,38 @@ use crate::{
     graph::{
         edge::EdgeTypeIndex,
         edge_store::{
-            adjacency_matrix_with_cached_attributes::GetWeightedAdjacencyMatrix,
+            adjacency_matrix_with_cached_attributes::{GetCachedAttributesOfAdjacencyMatrix, GetWeightedAdjacencyMatrix},
             weighted_adjacency_matrix::WeightedAdjacencyMatrix, EdgeStore, EdgeStoreTrait,
         },
     },
 };
 
 pub(crate) trait GetAdjacencyMatrixCachedAttributes {
-    fn try_adjacency_matrix_transpose_ref(
+    fn try_transposed_adjacency_matrix_ref(
         &mut self,
         edge_type_index: &EdgeTypeIndex,
     ) -> Result<&WeightedAdjacencyMatrix, GraphComputingError>;
 
-    fn adjacency_matrix_transpose_ref_unchecked(
+    fn transposed_adjacency_matrix_ref_unchecked(
         &mut self,
         edge_type_index: &EdgeTypeIndex,
     ) -> &WeightedAdjacencyMatrix;
 }
 
 impl GetAdjacencyMatrixCachedAttributes for EdgeStore {
-    fn adjacency_matrix_transpose_ref_unchecked(
+    fn transposed_adjacency_matrix_ref_unchecked(
         &mut self,
         edge_type_index: &EdgeTypeIndex,
     ) -> &WeightedAdjacencyMatrix {
-        self.adjacency_matrices_ref()[*edge_type_index].weighted_adjacency_matrix_ref()
+        self.adjacency_matrices_mut_ref()[*edge_type_index].transposed_weighted_adjacency_matrix_ref().unwrap()
     }
 
-    fn try_adjacency_matrix_transpose_ref(
+    fn try_transposed_adjacency_matrix_ref(
         &mut self,
         edge_type_index: &EdgeTypeIndex,
     ) -> Result<&WeightedAdjacencyMatrix, GraphComputingError> {
-        match self.adjacency_matrices_ref().get(*edge_type_index) {
-            Some(adjacency_matrix) => Ok(adjacency_matrix.weighted_adjacency_matrix_ref()),
+        match self.adjacency_matrices_mut_ref().get_mut(*edge_type_index) {
+            Some(adjacency_matrix) => Ok(adjacency_matrix.transposed_weighted_adjacency_matrix_ref()?),
             None => Err(LogicError::new(
                 LogicErrorType::EdgeTypeMustExist,
                 format!("No edge type for edge type index: {}", edge_type_index),
